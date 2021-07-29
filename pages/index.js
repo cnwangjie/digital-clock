6fecde4d773d2f4fd9b21d788f4347e1736c0418
Wang Jie <i@i8e.net>
2021-07-30 05:29:05 +0800

    feat: add yiyan & weather

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
-import {useRaf} from 'react-use';
+import {useEvent, useInterval, useMouse, useRaf, useUpdate} from 'react-use';
 import {useState} from 'react'
+import useSWR from 'swr';
 
 dayjs.extend(LocalizedFormat)
 
@@ -34,14 +35,54 @@ const FullscreenBtn = () => {
   )
 }
 
+const Header = () => {
+  const update = useUpdate()
+
+  useInterval(update, 1000)
+
+  return <span>
+    {dayjs().format('ll ddd')}
+  </span>
+}
+
+const YiYan = () => {
+  const { data, mutate } = useSWR('https://v1.hitokoto.cn/', {
+    refreshInterval: 60000,
+  })
+
+  if (typeof window !== 'undefined') useEvent('click', () => mutate(), window)
+
+  if (!data) return null
+
+  return <div>
+    <div>{data?.hitokoto}</div> <div className="float-right">——{data?.from}</div>
+  </div>
+}
+
+const fetchText = url => fetch(url).then(res => res.text())
+
+const Weather = () => {
+  const { data, mutate } = useSWR('https://wttr.in/?format=3', fetchText, {
+    refreshInterval: 3600e3,
+  })
+
+  return <div>
+    {data}
+  </div>
+}
+
 export default function Home() {
   return (
     <div className="select-none absolute inset-0 bg-black text-white flex justify-center items-center">
-      <div className="absolute left-8 top-8" style={{ fontSize: 80 }}>
-        {dayjs().format('ll ddd')}
+      <div className="absolute left-8 top-8 right-8 flex justify-between" style={{ fontSize: 80 }}>
+        <Header />
+        <Weather />
       </div>
       <div className="font-mono font-bold" style={{ fontSize: 240 }}>
         <Time />
+        <div className="break-words h-4" style={{ bottom: '25%', fontSize: 36, width: 1200 }}>
+          <YiYan />
+        </div>
       </div>
       <div className="absolute right-8 bottom-8">
         <FullscreenBtn />
