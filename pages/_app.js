c1d313af66e99e5cbf76d9992cbe8c83eafb04ca
Wang Jie <i@i8e.net>
2021-11-21 02:23:28 +0800

    chore: add GA

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,35 @@ import '../styles/globals.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Screen Digital Clock</title>
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Screen Digital Clock</title>
 
-      <link rel='manifest' href='/manifest.json' />
-    </Head>
-    <Component {...pageProps} />
-  </>
+        <link rel="manifest" href="/manifest.json" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3TCWGW2P8F"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3TCWGW2P8F');
+          `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
